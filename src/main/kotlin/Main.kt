8b8e469800546e93f371890d2b58105cae297727
jmt2110 <jthomas863@students.mchenry.edu>
2022-01-26 18:30:36 -0600

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,11 @@
+fun main(args: Array<String>) {
+    var supplies = arrayOf("hot dogs", "buns", "mustard", "relish", "onion")
+
+    var count = 0
+    println("Supplies for a hot dog stand")
+    while (count < supplies.size){
+        println(supplies[count])
+        count += 1
+    }
+
+}
